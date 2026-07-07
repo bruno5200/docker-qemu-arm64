3b60c85e004e6449d60965dafd3cded344aab820
bruno5200 <men520054@hotmail.com>
2026-07-07 00:27:54 -0400

    Update installation scripts and alpine versión

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-pkg update
+pkg update -y
+pkg upgrade -y
 pkg install -y expect curl qemu-utils qemu-common qemu-system-x86_64-headless openssh
 
 
@@ -11,8 +12,27 @@ fi
 
 source config.env
 
+echo "[*] Prepare installation directory..."
+mkdir -p "$env(INSTALLATION_DIR)"
+cp startqemu.sh "$INSTALLATION_DIR/"
+cp ssh2qemu.sh "$INSTALLATION_DIR/"
+
+echo "[*] alpine ISO Verification..."
+if [ ! -f "$INSTALLATION_DIR/alpine.iso" ]; then
+    curl -L -o "$INSTALLATION_DIR/alpine.iso" "$ALPINE_ISO_URL"
+fi
+
+echo "[*] Generate SSH Keys for the VM..."
+rm -f "$INSTALLATION_DIR/qemukey" "$INSTALLATION_DIR/qemukey.pub"
+ssh-keygen -b 2048 -t rsa -N "" -f "$INSTALLATION_DIR/qemukey"
+
+echo "[*] Create a clean virtual disk..."
+rm -f "$env(INSTALLATION_DIR)/alpine.img"
+qemu-img create -f qcow2 "$env(INSTALLATION_DIR)/alpine.img" "$DISK_SIZE"
+
 expect -f installqemu.expect
 
-echo "to start your new vm:"
-echo "  cd \$INSTALLATION_DIR && ./startqemu.sh"
-
+echo ""
+echo "Process finish!"
+echo "To start your new vm:"
+echo "  cd $INSTALLATION_DIR && ./startqemu.sh"
